1793b168c497d2dc235651be4202016c15113b7b
ArbaazKhan1 <bazzy_94@yahoo.com>
2024-07-07 16:10:37 -0500

    added clients

--- src/components/Clients.jsx
+++ src/components/Clients.jsx
@@ -1,8 +1,18 @@
 import React from 'react'
+import { clients } from '../constants'
+import styles from '../style'
 
 const Clients = () => {
   return (
-    <div>Clients</div>
+    <section className={` ${styles.flexCenter} my-4`}>
+      <div className={` ${styles.flexCenter} flex-wrap w-full`}>
+        {clients.map((clinet) => (
+          <div key={clinet.id} className={`flex-1 ${styles.flexCenter} sm:min-w-[192px] min-w-[120px]`}>
+            <img src={clinet.logo} alt={clinet} className={`sm:w-[192px] w-[100px] object-contain cursor-pointer hover:filter hover:invert`} />
+          </div>
+        ))}
+      </div>
+    </section>
   )
 }
 
